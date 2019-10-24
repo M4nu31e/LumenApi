a2c11b654464f420767aa7009d95246e36efde50
Manuel Söllner <manuel.soellner@internetx.com>
2019-10-24 16:24:28 +0200

    Initial Commit docker-compose.yml angepasst

---
+++ app/Services/BackendConnectionClient/BackendConnection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manuel.Soellner
+ * Date: 24.10.2019
+ * Time: 15:44
+ */
+
+namespace App\Services\BackendConnectionClient;
+
+class BackendConnection
+{
+
+}
