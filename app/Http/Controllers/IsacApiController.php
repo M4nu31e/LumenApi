31df426868934ee9db13972ec1245aea22a5ed1b
Manuel Söllner <manuel.soellner@internetx.com>
2019-10-24 15:42:38 +0200

    Initial Commit format

--- app/Http/Controllers/IsacApiController.php
+++ app/Http/Controllers/IsacApiController.php
@@ -2,15 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\IsacApiService;
-use Illuminate\Http\Response;
-use Laravel\Lumen\Http\ResponseFactory;
-
 class IsacApiController extends Controller
 {
-
-
-
-
 
 }
